99f9bdbc835050c69d19672beaaf3a9e6b038f81
jakub-havlik <havlik9@gmail.com>
2022-05-12 11:23:55 +0200

    converted meet app into a pwa

--- src/index.js
+++ src/index.js
@@ -5,22 +5,18 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
+//import * as atatus from 'atatus-js';
 
+// monitoring with atatus
 atatus.config('6fa23f80650c4d0cb7da3e1b97bf1fb5').install();
 
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
